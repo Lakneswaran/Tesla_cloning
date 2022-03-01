ce32e7d2824de6473b98a289d3eb2760de6f9001
Lakneswaran <90680982+Lakneswaran@users.noreply.github.com>
2022-03-01 15:09:32 +0100

    scroll libary js installed

--- script/tesla-clone.js
+++ script/tesla-clone.js
@@ -1,3 +1,5 @@
+import ScrollOut from "scroll-out";
+
 const overlay = document.querySelector('.overlay');
 const overlayClose = document.querySelector('.overlay__close');
 
@@ -10,3 +12,13 @@ openMenu.forEach(menuBtn => {
 overlayClose.addEventListener('click', (e) => {
     overlay.classList.remove('show');
 });
+
+ScrollOut({
+    scrollingElement: ".container",
+    onShown: function(el) {
+      el.animate([{ opacity: .5 }, { opacity: 1 }], 1500);
+    },
+    onHidden: function(el) {
+      el.animate([{ opacity: 1 }, { opacity: .5 }], 1500);
+    }
+  });
